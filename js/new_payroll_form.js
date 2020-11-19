d2680a8ffe16e4d703e1ec8920ad425f8a2796fe
leena0203 <leenasarode02@gmail.com>
2020-11-19 21:23:24 +0530

    Added: validate first name using regex

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -1,10 +1,3 @@
-/* UC7 */
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function(){
-    output.textContent = salary.value;
-});
 /* UC8 */
 class EmployeePayrollData {
 
@@ -61,4 +54,25 @@ const createEmployeePayrollData = () => {
 const getInputValueById = (id) => {
     let value = document.querySelector(id).value;
     return value;
-} 
+}
+/* UC9 */ 
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function(){
+    output.textContent = salary.value;
+});
+const textError = document.querySelector('.text-error');
+const name = document.querySelector('#name');
+name.addEventListener('input', function(){
+        if(name.value.length == 0){
+        textError.textContent = "";
+        return;
+    }
+    try{
+        (new EmployeePayrollData()).name = name.value;
+        textError.textContent = "";
+    }catch(e){
+        textError.textContent = e;
+    }
+});
