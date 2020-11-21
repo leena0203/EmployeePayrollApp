74b5837291dbea347a12c4c8d9aca56732c479a3
leena0203 <leenasarode02@gmail.com>
2020-11-21 17:49:53 +0530

    Added: to remove the employee from payroll

--- js/new_home.js
+++ js/new_home.js
@@ -1,6 +1,6 @@
 let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
-    
+   // localStorage.clear();
     empPayrollList = getEmployeePayrollDataFromStorage();
     document.querySelector('.emp-count').textContent = empPayrollList.length;
     createInnerHtml();
@@ -25,8 +25,8 @@ const createInnerHtml = () => {
     <td>${empPayrollData._salary}</td>
     <td>${stringifyDate(empPayrollData._startDate)}</td>
     <td>
-        <img name = "${empPayrollData._id}" src = "../assets/icons/delete-black-18dp.svg" onclick = "update(this)" alt = "edit">
-        <img name = "${empPayrollData._id}" src = "../assets/icons/create-black-18dp.svg" onclick = "remove(this)" alt = "delete">
+        <img id = "${empPayrollData._id}" src = "../assets/icons/create-black-18dp.svg" onclick = "update(this)" alt = "edit">
+        <img id = "${empPayrollData._id}" src = "../assets/icons/delete-black-18dp.svg" onclick = "remove(this)" alt = "delete">
     </td>
     </tr>
     `;
@@ -40,4 +40,13 @@ const getDeptHtml = (deptList) => {
         deptHtml = `${deptHtml} <div class ='dept-label'>${dept}</div>`;
     }
     return deptHtml;
+}
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id = node.id);
+    if( !empPayrollData) return;
+    const index = empPayrollList.map(empData => empData._id).indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
 }
